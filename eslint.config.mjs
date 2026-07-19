550dcbb1fa92742eef88a5e78609caa8041b1377
Troy Washington <103076891+TVW96@users.noreply.github.com>
2026-07-18 20:12:29 -0700

    add @eslint/css

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,24 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import css from "@eslint/css";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { 
+    files: ["**/*.{js,mjs,cjs,css}"], 
+    plugins: { js, css }, 
+    extends: ["js/recommended", "css/recommended"], 
+    languageOptions: { globals: globals.browser }, 
+    language: "css/css", 
+    rules: {
+      "css/no-duplicate-imports": "error",
+      "css/no-empty-blocks": "warn",
+      "css/no-duplicate-selectors": "error",
+      "css/no-empty-rulesets": "warn",
+    },
+  },
+  { 
+    files: ["**/*.js"], 
+    languageOptions: { sourceType: "script" } 
+  },
 ]);
